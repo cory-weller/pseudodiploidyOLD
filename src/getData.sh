244313966b249726ff56d000ae5164137ce1624b
Cory <cory.a.weller@gmail.com>
2021-12-21 20:22:00 +0000

    update getData.sh

--- src/getData.sh
+++ src/getData.sh
@@ -2,22 +2,31 @@
 
 # script to retrieve raw data set
 
-# download SGD_features
-if [[ ! -f data/external/SGD_features.tab ]]; then
-    wget -O data/external/SGD_features.tab http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab
-else
-    echo "SGD_features already downloaded"
-fi
-features_checksum=$(sha512sum data/external/SGD_features.tab | cut -d " " -f 1)
-if [[ ${features_checksum} != "cc3c54abb0106993b4d09227f4748e46460b4a7614518b6513863d7c175d9e9bdde773a676729437f7c2cba622cc40c98dbc83bf38b84149e05aedf056639373" ]]; then
-    echo "WARNING: SGD_features.tab checksum failed, contents may differ from those originally used"
-else
-    echo "SGD_features.tab checksum passed"
-fi
 
+getData () {
+    filePath=${1}
+    fileURL=${2}
+    fileSHA512=${3}
 
-# if [[ ! -f data/external/filename.txt ]]; then
-#     wget -O data/external/filename.txt http://url-to-file/filename.txt
-# else
-#     echo "filename already downloaded"
-# fi
+    if [[ ! -f ${filePath} ]]; then
+        wget -O ${filePath} ${fileURL}
+    else
+        echo "${filePath} already downloaded"
+    fi
+    checksum=$(sha512sum ${filePath} | cut -d " " -f 1)
+    if [[ ${checksum} != "${fileSHA512}" ]]; then
+        echo "WARNING: ${filePath} checksum failed, contents may differ from those originally used"
+    else
+        echo "${filePath} checksum passed"
+    fi
+}
+
+getData \
+    "data/external/SGD_features.tab" \
+    "http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab" \
+    "cc3c54abb0106993b4d09227f4748e46460b4a7614518b6513863d7c175d9e9bdde773a676729437f7c2cba622cc40c98dbc83bf38b84149e05aedf056639373"
+
+getData \
+    "src/R.sif" \
+    "https://onedrive.live.com/download?cid=77DD71E598E5B51B&resid=77DD71E598E5B51B%2119110&authkey=AH7s36_oIlV8uIU" \
+    "7153e1498b86983ac83057d0dd7c7563b5fd67ad4aec2d348b23004c3e1733cc17a46a2ef02e076c315ba72594b8c9678f1ce657ac031d534a8ff53da5b89af6"
